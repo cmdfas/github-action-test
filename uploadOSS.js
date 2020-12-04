04d960450d89de7ebc8a7b852553313274ce0038
cmdfas <cmdfas@gmail.com>
2020-12-04 11:26:06 +0800

    person one

--- uploadOSS.js
+++ uploadOSS.js
@@ -1,11 +1,21 @@
 const OSS = require('ali-oss')
 
+// 公司
+// const client = OSS({
+//     accessKeyId: 'w122A7d2d2NIV7Pf',
+//     accessKeySecret: 'PO5EIECFaVC4KOWbwyPa7TvZULTPAl',
+//     bucket: 'wemark',
+//     region: 'oss-cn-shenzhen',
+//     timeout: 3000000
+// })
+
+// 个人
 const client = OSS({
-    accessKeyId: 'w122A7d2d2NIV7Pf',
-    accessKeySecret: 'PO5EIECFaVC4KOWbwyPa7TvZULTPAl',
-    bucket: 'wemark',
-    region: 'oss-cn-shenzhen',
-    timeout: 3000000
+    accessKeyId: 'I24bUrWouhPLOpLj',
+    accessKeySecret: 'LeC3muidG0Q64UGGEorxwjjuyNYO53',
+    bucket: 'us-release-exe',
+    region: 'oss-us-west-1',
+    timeout: 300000
 })
 
 /**
@@ -13,18 +23,20 @@ const client = OSS({
  * @param {*} src
  * @param {*} dist
  */
-// async function putOSS(src, dist) {
-//     try {
-//         //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
-//         await client.put(dist, src);
-//         console.log('上传OSS成功', src, dist);
-//     } catch (e) {
-//         console.log(`失败，本地${src} -> 远程${dist}`)
-//         console.log(e);
-//     }
-// }
+async function putOSS(src, dist) {
+    try {
+        //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
+        await client.put(dist, src);
+        console.log('上传OSS成功', src, dist);
+    } catch (e) {
+        console.log(`失败，本地${src} -> 远程${dist}`)
+        console.log(e);
+    }
+}
 
 // putOSS('./release/红旗直播-1.2.0.pkg', 'release/红旗直播-1.2.0.pkg')
+
+// putOSS('./package.json', 'release/package.json')
 
 const progress = (p, _checkpoint) => {
     console.log(p); // Object的上传进度。
